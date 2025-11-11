201bc280b625b3e2a5313cbfa949ce4940e69939
tansudasli <tansudasli@gmail.com>
2025-11-11 12:36:15 +0300

    some key injections

--- core/dictionary-complex.py
+++ core/dictionary-complex.py
@@ -1,26 +1,19 @@
+#key=value format
 contacts = dict(
-    Jason={
-        'phone': '555-0267',
-        'email': 'jason@'},
-    Carl={
-        'phone': '555-0978',
-        'email': 'carl@'}
+    Jason = { 'phone': '555-0267', 'email': 'jason@'},
+    Carl  = { 'phone': '555-0978', 'email': 'carl@'}
 )
 
-#
-# contacts = {
-#     'Jason': {
-#         'phone': '555-0267',
-#         'email': 'jason@'
-#     },
-#     'Carl': {
-#         'phone': '555-0978',
-#         'email': 'carl@'
-#     }
+# 'key':value format
+# contacts = { 
+#       #key     value
+#       'Jason': {'phone': '555-0267', 'email': 'jason@' },
+#       'Carl' : {'phone': '555-0978', 'email': 'carl@' }   
 # }
-#
 
+# + operator on strings, concat with space and newline!
 for contact in contacts:
-    print('keys are: {} '.format(contact))
-    print('\t email: ' + contacts[contact]['email'])
+    print('key: ' + contact + '\t', contacts[contact]['email'], end=' \n')          
+    # print('key: {} '.format(contact), end=' ')
+    # print('\t email: ' + contacts[contact]['email'])               
 
