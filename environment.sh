6c58832b811527b2f9f578dd2ac45ec064a54b31
tansudasli <tansudasli@gmail.com>
2025-11-16 00:52:43 +0300

    restructuring

--- environment.sh
+++ environment.sh
@@ -2,6 +2,7 @@
 conda create -n core python=3
 
 conda install -n core ipykernel
-conda install -n core numpy
+conda install -n core numpy scipy #pandas matplotlib scikit-learn jupyterlab seaborn
+
 
 conda activate core 
